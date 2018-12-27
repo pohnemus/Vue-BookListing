dacf5f8a7243181c7258dff006ccc4a9a076e084
Phil Ohnemus <ohnemus.phil@principal.com>
2018-12-27 17:37:43 -0600

    module 5

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,12 +2,14 @@
   <div>
     <h1>{{ title }}</h1>
     <ul>
-      <li v-for="book in books" :key="book.key">{{book.title}}:{{book.author}}</li>
+      <book-item v-for="book in books" :key="book.key" :book="book"></book-item>
     </ul>
   </div>
 </template>
 
 <script>
+  import BookItem from '@/components/BookItem';
+
   export default {
     name: 'BookList',
     data() {
@@ -20,6 +22,7 @@
         ]
       };
     },
+    components: {BookItem}
   };
 </script>
 
